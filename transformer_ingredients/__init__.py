033456c83c1820359aab4fb644677706b2fa2752
OleguerCanal <oleguer.canal@hotmail.com>
2022-08-07 09:25:59 +0200

    Added common utils

--- transformer_ingredients/__init__.py
+++ transformer_ingredients/__init__.py
@@ -1,3 +1,4 @@
 from .transformer_encoder import TransformerEncoder, TransformerEncoderLayer
 from .transformer_decoder import TransformerDecoder, TransformerDecoderLayer
 from .transformer import Transformer
+from .utilis import set_seed
