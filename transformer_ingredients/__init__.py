7a43a1b62e10f8a85e4796f7d08c7b185608c10f
OleguerCanal <oleguer.canal@hotmail.com>
2022-08-21 08:34:48 +0000

    Made collate_fn accessible

--- transformer_ingredients/__init__.py
+++ transformer_ingredients/__init__.py
@@ -2,4 +2,4 @@ from .modules import Linear
 from .transformer_encoder import TransformerEncoder, TransformerEncoderLayer
 from .transformer_decoder import TransformerDecoder, TransformerDecoderLayer
 # from .transformer import Transformer
-from .utils import set_seed
+from .utils import set_seed, collate_fn
