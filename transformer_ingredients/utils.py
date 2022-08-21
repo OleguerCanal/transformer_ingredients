f516b01c0268608969eb063595d9920e6b52c318
OleguerCanal <oleguer.canal@hotmail.com>
2022-08-21 08:33:47 +0000

    Added collate_fn

--- transformer_ingredients/utils.py
+++ transformer_ingredients/utils.py
@@ -5,7 +5,64 @@ import torch
 
 
 def set_seed(seed):
+    """Set the random seed for all used libraries
+    """
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+
+
+def collate_fn(batch, pad_id: int = 0):
+    r"""
+    Functions that pad to the maximum sequence length
+
+    Args:
+        batch (tuple): tuple contains input and target tensors
+        pad_id (int): identification of pad token
+
+    Returns:
+        seqs (torch.FloatTensor): tensor contains input sequences.
+        target (torch.IntTensor): tensor contains target sequences.
+        seq_lengths (torch.IntTensor): tensor contains input sequence lengths
+        target_lengths (torch.IntTensor): tensor contains target sequence lengths
+    """
+    def seq_length_(p):
+        return len(p[0])
+
+    def target_length_(p):
+        return len(p[1])
+
+    # sort by sequence length for rnn.pack_padded_sequence()
+    batch = sorted(batch, key=lambda sample: sample[0].size(0), reverse=True)
+
+    seq_lengths = [len(s[0]) for s in batch]
+    target_lengths = [len(s[1]) - 1 for s in batch]
+
+    max_seq_sample = max(batch, key=seq_length_)[0]
+    max_target_sample = max(batch, key=target_length_)[1]
+
+    max_seq_size = max_seq_sample.size(0)
+    max_target_size = len(max_target_sample)
+
+    feat_size = max_seq_sample.size(1)
+    batch_size = len(batch)
+
+    seqs = torch.zeros(batch_size, max_seq_size, feat_size)
+
+    targets = torch.zeros(batch_size, max_target_size).to(target)
+    targets.fill_(pad_id)
+
+    for x in range(batch_size):
+        sample = batch[x]
+        tensor = sample[0]
+        target = sample[1]
+        seq_length = tensor.size(0)
+
+        seqs[x].narrow(0, 0, seq_length).copy_(tensor)
+        targets[x].narrow(0, 0, len(target)).copy_(target)
+
+    seq_lengths = torch.IntTensor(seq_lengths)
+    target_lengths = torch.IntTensor(target_lengths)
+
+    return seqs, targets, seq_lengths, target_lengths
